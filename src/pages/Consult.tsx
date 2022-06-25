44b347f75b9d339c7d747a5da25afa0d4a1d3b51
hendriprst <hendriprastio99@gmail.com>
2022-06-25 12:56:17 +0700

    create consult page

---
+++ src/pages/Consult.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../components/layout/Footer'
+import Navbar from '../components/layout/Navbar'
+
+export default function Consult() {
+  return (
+  <>
+    <Navbar />
+      <h1>Hello world</h1>
+    <Footer />
+  </>
+  )
+}
