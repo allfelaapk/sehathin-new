38fc1c9d8bfdd82fb3f5d2e8ceda0ec122c70d94
Achmad Kelvin <a.kelvintri@gmail.com>
2022-07-07 13:03:15 +0700

    About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar';
+import './About.css';
 
 export default function About() {
   return (
     <>
       <Navbar />
-      <h1>About</h1>
+      <div className="jumbotron">
+        <h1>Hello, We’re SehaThin.</h1>
+        <h2>OUR MISSION : Provide your need to achieve healthy body goals</h2>
+      </div>
+      <div className="descript">
+        <h3>Gain weight with SehaThin</h3>
+        <p>We know that gaining weight is not easy. But by using SehaThin, you will have all the tools you need to make it easier for you to gain weight healthily. </p>
+        <p>Tracking nutrition and physical activity is proven to be very effective in managing your healthy lifestyle. The more you track your nutrition, the higher your chances of gaining weight. That's why every successful gain weight program recommends you keeping a food and/or exercise diary. </p>
+        <p>Recording all meals and meals eaten in the absence of the necessary tools can be a tedious task, and most likely a tedious task. So, we are SehaThin strives to create the best conditions for recording food. After all, the easier it is for you to keep a food diary, the higher the chances that you will not turn off the chosen path and successfully reach your goal.</p>
+      </div>
       <Footer />
     </>
   );
