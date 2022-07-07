1fc2d08023ff9ac9d3ada904b6062d5fed1e379f
Achmad Kelvin <a.kelvintri@gmail.com>
2022-07-07 13:26:44 +0700

    Sort up

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar';
-import './About.css';
+import '../assets/styles/About.css';
 
 export default function About() {
   return (
