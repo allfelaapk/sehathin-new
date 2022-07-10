a8da14af8747c0e1892361b07b2bb2ae29a844a2
allfelaapk <alfelaaisyah21@gmail.com>
2022-07-10 19:25:53 +0700

    update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../src/App.css';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Typography } from '@mui/material';
 import NavRoutes from './routes/NavRoutes';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+  },
+});
 
 function App() {
   return (
     <div>
-      <NavRoutes />
+      <ThemeProvider theme={theme}>
+        <NavRoutes />
+      </ThemeProvider>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,25 +3,12 @@ import Box from '@mui/material/Box';
 import { Typography, Card, Avatar, CardContent } from '@mui/material';
 import image from '../assets/hero-image.svg';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar';
 import { logout } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 import FormBMI from '../components/formBmi/index';
-
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Nunito',
-      'Roboto',
-      'Helvetica Neue',
-      'Arial',
-      'sans-serif',
-      'Poppins',
-    ].join(','),
-  },
-});
 
 // let theme = createTheme();
 // theme = responsiveFontSizes(theme);
@@ -42,6 +29,15 @@ export default function HeroSection() {
     await logout();
     navigate('/');
   }
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
   return (
     <div style={{ width: '100%' }}>
       <Navbar />
@@ -59,52 +55,51 @@ export default function HeroSection() {
         logout
       </Button>
       {/* box for hero  */}
-      <ThemeProvider theme={theme}>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+          p: 5,
+          m: 5,
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+          justifyContent: 'space-evenly',
+        }}
+      >
         <Box
+          component="img"
           sx={{
-            display: 'flex',
-            flexDirection: {
-              xs: 'column',
-              md: 'row',
-            },
-            p: 5,
-            m: 5,
-            bgcolor: 'background.paper',
-            borderRadius: 1,
-            justifyContent: 'space-evenly',
+            height: 1000,
+            width: 500,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
           }}
-        >
-          <Box
-            component="img"
-            sx={{
-              height: 1000,
-              width: 500,
-              maxHeight: { xs: 233, md: 167 },
-              maxWidth: { xs: 350, md: 250 },
-            }}
-            alt="hero image"
-            src={image}
-          />
+          alt="hero image"
+          src={image}
+        />
+        <div>
+          {' '}
+          <Typography variant="h3">Set your healthy body goals.</Typography>
           <div>
-            {' '}
-            <ThemeProvider theme={theme}>
-              <Typography variant="h3">Set your healthy body goals.</Typography>
-              <Typography variant="body1">
-                SehaThin combines Meal Planner, Calorie counter, Nutrition food
-                suggestions, Discussion and Doctor Consult in one place to
-                provide you achieve it.
-              </Typography>
-            </ThemeProvider>
-            <Button
-              // classname={classes.root}
-              variant="contained"
-              sx={{ backgroundColor: '#aedbce', color: 'black' }}
-            >
-              Get Started
-            </Button>
+            <Typography variant="body1">
+              SehaThin combines Meal Planner, Calorie counter, Nutrition food
+              suggestions, Discussion and Doctor Consult in one place to provide
+              you achieve it.
+            </Typography>
           </div>
-        </Box>
-      </ThemeProvider>
+          <Button
+            // classname={classes.root}
+            variant="contained"
+            sx={{ backgroundColor: '#aedbce', color: 'black' }}
+          >
+            Get Started
+          </Button>
+        </div>
+      </Box>
 
       <div>
         {/* box for section2 */}
@@ -190,6 +185,94 @@ export default function HeroSection() {
               </Card>
             </div>
           </div>
+        </Box>
+      </div>
+      <div>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            p: 1,
+            m: 1,
+            bgcolor: 'background.paper',
+            borderRadius: 1,
+          }}
+        >
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         </Box>
       </div>
       <Footer />
